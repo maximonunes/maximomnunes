ddfe6deaa0a57e382df2436eff87b64d646f9aa0
maximonunes <a22408741@alunos.ulht.pt>
2025-11-17 13:13:51 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      
+      {/* === HEADER COM OS LINKS === */}
+      <header className="flex flex-col items-center gap-4 py-6">
+        <h1 className="text-2xl font-semibold dark:text-white">React & Next.js</h1>
+
+        <nav className="flex gap-4 text-lg">
+          <Link href="/">Intro</Link>
+          <Link href="/sobre">Sobre</Link>
+          <Link href = "/caracteristicas">Caracteristícas</Link>
+          <Link href = "/tecnologias">Tecnologiasn</Link>
+        </nav>
+      </header>
+      {/* =========================== */}
+
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -12,6 +27,7 @@ export default function Home() {
           height={20}
           priority
         />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
@@ -34,6 +50,7 @@ export default function Home() {
             center.
           </p>
         </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
@@ -50,6 +67,7 @@ export default function Home() {
             />
             Deploy Now
           </a>
+
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -63,4 +81,5 @@ export default function Home() {
     </div>
   );
 }
+
 
