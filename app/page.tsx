65b226b0af266d6a2308091894e1f1add7ebfc79
maximonunes <a22408741@alunos.ulht.pt>
2025-11-20 11:46:34 +0000

    Updated everything

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import MagiaDoJSX from "@/components/MagiaDoJSX"; 
-
+import "./globals.css"; 
 
 export default function Home() {
   return (
